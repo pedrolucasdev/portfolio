b6c71dff44c074dcec994ab0b0fb7237f00a6e34
Pedro Rocha <68920103+pedrolucasdev@users.noreply.github.com>
2024-10-24 23:21:05 +0100

    Added google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,9 +15,11 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const tag = process.env.NEXT_PUBLIC_GA ?? "";
   return (
     <html lang="en">
       <body className={`${inter.className} antialiased`}>{children}</body>
+      <GoogleAnalytics gaId={tag} />
     </html>
   );
 }
